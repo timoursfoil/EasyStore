94d0d232d4153a3d22b7495a2427b926a2cb1b4b
timoursfoil <thewesleyhorton@gmail.com>
2025-07-01 12:16:16 -0400

    Added comments to the Manager

--- src/manager/EasyStoreManager.server.lua
+++ src/manager/EasyStoreManager.server.lua
@@ -5,7 +5,7 @@ local Players = game:GetService("Players")
 local ServerScriptService = game:GetService("ServerScriptService")
 local EasyStore = require(ServerScriptService.Data.EasyStore)
 
-local function Leaderstats(player:Player)
+local function Leaderstats(player:Player) -- Creates leaderstats with the Gold value.
 	local profile = EasyStore:GetProfileofPlayer(player)
 	local oldleader = player:FindFirstChild("leaderstats")
 	if oldleader then
@@ -22,19 +22,19 @@ local function Leaderstats(player:Player)
 end
 
 Players.PlayerAdded:Connect(function(player)
-	EasyStore:LoadProfile(player)
-	Leaderstats(player)
+	EasyStore:LoadProfile(player) -- Loads the players profile
+	Leaderstats(player) -- Create leaderstats for player
 	task.wait(2)
 	while true do
-		local gold = EasyStore:Get(player, "Gold")
-		EasyStore:Set(player, "Gold", gold + 10)
-		Leaderstats(player)
+		local gold = EasyStore:Get(player, "Gold") -- Gets the Gold value
+		EasyStore:Set(player, "Gold", gold + 10) -- Adds 10 to the gold value. (Use :Increment as a better way)
+		Leaderstats(player) -- Updates leaderstats
 		task.wait(2)
 	end
 end)
 Players.PlayerRemoving:Connect(function(player)
-	EasyStore:Release(player)
+	EasyStore:Release(player) -- Releases the players profile on leave
 end)
 game:BindToClose(function()
-	EasyStore:ReleaseAll()
+	EasyStore:ReleaseAll() -- Releases all profiles on game Close
 end)
